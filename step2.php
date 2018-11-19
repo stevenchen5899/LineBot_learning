ad500dd7ae154faf20b2d9a2d6d945859c597cd1
stevenchen5899 <t0938139311@gmail.com>
2018-11-19 12:13:54 +0800

    Update step2.php

--- step2.php
+++ step2.php
@@ -21,7 +21,7 @@
  fwrite($myfile, "\xEF\xBB\xBF".json_encode($response)); //在字串前面加上\xEF\xBB\xBF轉成utf8格式
 
   $header[] = "Content-Type: application/json";
-  $header[] = "Authorization: Bearer gd1gyH+Pc5TROu9ku5u/5tDvFnffsU8nXU69zXuhTgE0dIS5nVGmx9Js8PwijeUqgFuwWXzyJ14/N5FUmp/UXsmSJbUsxMGA6AW1gozlf6cbEgSGLiC02BEaRa5wUSqE7df8FOANP1WjPW8Mh/TgtwdB04t89/1O/w1cDnyilFU=";
+  $header[] = "Authorization: Bearer iewVIXyfpmUm9qSF230NhA9kajJ5b5kSHVUL669amxxIiJh9xTgIibV5suZ3ARtgp3PglEUjMQhyC6bvADvqLAF5W9PoKC2qh4rkamH4o1/ysN0K/gChyC0bt3w+DVXqqee2Fzj5IIK3vE9egpv8cwdB04t89/1O/w1cDnyilFU=";
   $ch = curl_init("https://api.line.me/v2/bot/message/push");
   curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
   curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($response));                                                                  
